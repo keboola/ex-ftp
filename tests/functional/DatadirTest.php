02ace39071856e11d3ef11d8e32e76305f5b18cc
Vojta Tůma <vojta@usetreno.cz>
2018-11-06 13:40:43 +0100

    Hack with timestamp for datadir tests

--- tests/functional/DatadirTest.php
+++ tests/functional/DatadirTest.php
@@ -5,7 +5,30 @@ declare(strict_types=1);
 namespace Keboola\FtpExtractor\FunctionalTests;
 
 use Keboola\DatadirTests\DatadirTestCase;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 
 class DatadirTest extends DatadirTestCase
 {
+    protected function setUp() :void
+    {
+        parent::setUp();
+
+        $files = (new Finder())->files()->in(__DIR__ . '/../ftpInitContent/');
+        $json = [];
+        foreach ($files as $file) {
+            /** @var SplFileInfo $file*/
+            $json[$file->getRelativePathname()] = $file->getMTime();
+        }
+
+        $fs = new Filesystem();
+        //dumping state where's nothing to update
+        $state = [
+            "dir1/recursive.bin" => $json["dir1/recursive.bin"],
+        ];
+        $fs->dumpFile(__DIR__ . '/nothing-to-update/source/data/in/state.json', json_encode($state));
+        $fs->dumpFile(__DIR__ . '/nothing-to-update/expected/data/out/state.json', json_encode($state));
+        $fs->dumpFile(__DIR__ . '/normal-download/expected/data/out/state.json', json_encode($state));
+    }
 }
