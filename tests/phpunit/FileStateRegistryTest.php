83bf3366ebb7a33ecc205b37050040392074dd3b
Vojta Tůma <ja@vojta-tuma.cz>
2018-11-16 09:41:44 +0100

    FileStateRegistry test improve

--- tests/phpunit/FileStateRegistryTest.php
+++ tests/phpunit/FileStateRegistryTest.php
@@ -10,42 +10,46 @@ use PHPUnit\Framework\TestCase;
 class FileStateRegistryTest extends TestCase
 {
     /**
-     * @dataProvider sameSecondUpdateDataProvider
      * @dataProvider firstRunDataProvider
      */
-    public function testRegistry(string $path, int $timestamp, FileStateRegistry $registry, bool $expected): void
+    public function testRegistry(array $files, FileStateRegistry $registry): void
     {
-        $this->assertSame($expected, $registry->shouldBeFileUpdated($path, $timestamp));
+        foreach ($files as $file) {
+            $this->assertSame(
+                $file['expected'],
+                $registry->shouldBeFileUpdated($file['name'], $file['timestamp']),
+                sprintf("Bad decision for %s with timestamp %s", $file['name'], $file['timestamp'])
+            );
+        }
     }
 
     public function firstRunDataProvider(): array
     {
-        $registry = $this->getRegistry(1000, []);
-
-        return [
-            ['dir1/files/1.txt', 900, $registry, false],
-            ['dir1/files/2.txt', 1000, $registry, true],
-            ['dir1/files/3.txt', 1002, $registry, true],
-            ['dir1/files/4.txt', 1005, $registry, true],
+        $firstRunFiles = [
+            ['name' => 'dir1/files/1.txt', 'timestamp' => 900, 'expected' => false],
+            ['name' => 'dir1/files/2.txt', 'timestamp' => 1000, 'expected' => true],
+            ['name' => 'dir1/files/3.txt', 'timestamp' => 1002, 'expected' => true],
+            ['name' => 'dir1/files/4.txt', 'timestamp' => 1005, 'expected' => true],
         ];
-    }
 
-    public function sameSecondUpdateDataProvider(): array
-    {
+        $sameSecondUpdateFiles = [
+            ['name' => '/dir2/file2.csv', 'timestamp' => 1000, 'expected' => false],
+            ['name' => '/dir2/file1.csv', 'timestamp' => 1000, 'expected' => false],
+            ['name' => '/dir2/file3.csv', 'timestamp' => 1000, 'expected' => true],
+            ['name' => '/dir2/file5.csv', 'timestamp' => 1000, 'expected' => true],
+            ['name' => '/dir3/file1.csv', 'timestamp' => 1001, 'expected' => true],
+            ['name' => '/dir3/file2.csv', 'timestamp' => 1001, 'expected' => true],
+            ['name' => '/dir5/file3.csv', 'timestamp' => 1005, 'expected' => true],
+        ];
+
         $lastFiles = [
             '/dir2/file1.csv',
             '/dir2/file2.csv',
         ];
-        $registry = $this->getRegistry(1000, $lastFiles);
 
         return [
-            ['/dir2/file2.csv', 1000, $registry, false],
-            ['/dir2/file1.csv', 1000, $registry, false],
-            ['/dir2/file3.csv', 1000, $registry, true],
-            ['/dir2/file5.csv', 1000, $registry, true],
-            ['/dir3/file1.csv', 1001, $registry, true],
-            ['/dir3/file2.csv', 1001, $registry, true],
-            ['/dir5/file3.csv', 1005, $registry, true],
+           'firstRun' => [$firstRunFiles, $this->getRegistry(1000, [])],
+           'secondRunWithLastFiles' => [$sameSecondUpdateFiles, $this->getRegistry(1000, $lastFiles)],
         ];
     }
 
