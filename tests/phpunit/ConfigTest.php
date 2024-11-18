c533de64ec8d9bdddc06f9102209b5e4779a61bb
Jiří Novák <jiri.novak@keboola.com>
2024-11-18 15:25:24 +0100

    fix: PST-1861 fix test after rebase

--- tests/phpunit/ConfigTest.php
+++ tests/phpunit/ConfigTest.php
@@ -335,7 +335,6 @@ class ConfigTest extends TestCase
             'timeout' => 60,
             'recurseManually' => false,
             'ignorePassiveAddress' => false,
-            'useRawListOptions' => true,
             'timestampsOnUnixListingsEnabled' => true,
         ];
 
